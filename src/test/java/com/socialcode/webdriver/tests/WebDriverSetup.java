b9cf2523d372a47db2a24cf531c6aa95e69fb9e5
anthonyc-sc <anthony.c@socialcodeinc.com>
2016-03-10 15:48:14 -0800

    Update Web Driver Setup

--- src/test/java/com/socialcode/webdriver/tests/WebDriverSetup.java
+++ src/test/java/com/socialcode/webdriver/tests/WebDriverSetup.java
@@ -26,14 +26,9 @@ public class WebDriverSetup {
     protected RemoteWebDriver driver;
 
     protected TestData data;
-
-    //protected final String prismURL = "https://beta.socialcode.com/advisor/#initiative";
     protected final String prismURL = System.getProperty("environment");
-
     protected final String environment = System.getProperty("env");
-    //protected final String environment = "prod";
     protected String platF = System.getProperty("platform");
-    //protected final String platF = "all";
     protected String dataFolder = System.getProperty("file.separator")+ "data" + System.getProperty("file.separator") + environment.toLowerCase() + System.getProperty("file.separator") + platF + System.getProperty("file.separator");
     protected List<String> cookie = null;
     protected String cookieName = "bouncer";
@@ -41,7 +36,12 @@ public class WebDriverSetup {
     protected String cookieExpiration = "Fri Mar 18 14:30:57 PDT 2016";
     protected String isSecure = "False";
     protected String browser  = System.getProperty("browser");
+    protected String browserVersion = System.getProperty("version");
     protected String hubURL = System.getProperty("hub");
+
+    //protected final String prismURL = "https://beta.socialcode.com/advisor/#initiative";
+    //protected final String platF = "all";
+    //protected final String environment = "prod";
 
     /**
      * Instantiates Web Driver object
@@ -57,11 +57,9 @@ public class WebDriverSetup {
             cookie.add(cookieExpiration);
             cookie.add(isSecure);
 
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.home") + "/chromedriver/chromedriver");
-          //  driver = new RemoteWebDriver(new URL("http://localhost:9515"), DesiredCapabilities.chrome());
             DesiredCapabilities capability = new DesiredCapabilities();
             capability.setBrowserName(browser);
-
+            capability.setVersion(browserVersion);
             driver = new RemoteWebDriver(new URL(hubURL),capability);
             driver.manage().window().maximize();
         } catch (Exception e) {
